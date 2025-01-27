4681e55bb3e6a04ed368034b845a85a01b6560db
rex.yu <jiafish@outlook.com>
2025-01-27 01:31:04 +0100

    fix bug

--- request_generator/test/request_run.py
+++ request_generator/test/request_run.py
@@ -1,8 +1,8 @@
 import requests
 import pickle
 
-payload1= {"user_id":1, "payload_size":[128, 128],"frequency":[2,1], "bandwidth":1000000,"app":["app1","app2"], "ms_chain":["2,ms1-1-1,1-2,1-3","2,ms2-1-1,1-2,1-3"]}
+payload1= {"user_id":1, "payload_size":[128, 128],"frequency":[2,1], "bandwidth":1000000,"app":["app1","app2"], "ms_chain":["2,service1-1-1,2-1","2,service1-1-1,2-2"]}
 
-response = requests.post(url="http://192.168.0.110:2356/run", headers={"Content-Type": "application/json"},data=pickle.dumps(payload1))
+response = requests.post(url="http://localhost:2333/run", headers={"Content-Type": "application/json"},data=pickle.dumps(payload1))
 
 print(pickle.loads(response.content))
