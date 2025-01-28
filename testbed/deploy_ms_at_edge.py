f5ef78bd5dcb71da281e3c79993e7b473e09d9ca
rex.yu <jiafish@outlook.com>
2025-01-28 14:34:53 +0100

    fix bug

--- testbed/deploy_ms_at_edge.py
+++ testbed/deploy_ms_at_edge.py
@@ -9,7 +9,8 @@ config_folder = "./k8s_converted/"
 
 with open(config_folder+ms+".yaml", "r") as f:
     config = list(yaml.safe_load_all(f))
-
+    
+config[1]["spec"]["replicas"] = 1
 config[1]["spec"]["template"]["spec"]["nodeName"] = edge
 
 with open(config_folder+ms+".yaml", "w") as f:
--- testbed/remove_ms.py
+++ testbed/remove_ms.py
@@ -11,7 +11,8 @@ os.system("kubectl delete -f "+config_folder+ms+".yaml")
 
 with open(config_folder+ms+".yaml", "r") as f:
     config = list(yaml.safe_load_all(f))
-
+    
+config[1]["spec"]["replicas"] = 0
 config[1]["spec"]["template"]["spec"]["nodeName"] = "dummy"
 
 with open(config_folder+ms+".yaml", "w") as f:
